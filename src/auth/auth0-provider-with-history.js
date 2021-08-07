386eabdc3e915e10f8a5ab8cc633d41135d89779
Petter Daae <petter.daae@gmail.com>
2021-08-07 16:06:47 +0200

    Send authenticated request to backend

--- src/auth/auth0-provider-with-history.js
+++ src/auth/auth0-provider-with-history.js
@@ -5,11 +5,11 @@ import { Auth0Provider } from "@auth0/auth0-react";
 const Auth0ProviderWithHistory = ({ children }) => {
     const domain = process.env.REACT_APP_AUTH0_DOMAIN;
     const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+    const audience = process.env.REACT_APP_AUTH0_AUDIENCE;
 
     const history = useHistory();
 
     const onRedirectCallback = (appState) => {
-        console.log(appState);
         history.push(appState?.returnTo || window.location.pathname);
     };
 
@@ -19,6 +19,7 @@ const Auth0ProviderWithHistory = ({ children }) => {
             clientId={clientId}
             redirectUri={window.location.origin}
             onRedirectCallback={onRedirectCallback}
+            audience={audience}
         >
             {children}
         </Auth0Provider>
