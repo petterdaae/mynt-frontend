ab68f8a5803a1f2b28e5b07e504aa9d7a919d063
Petter Daae <petter.daae@gmail.com>
2021-08-13 16:42:59 +0200

    Add ui for setting sbanken secrets

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import { Route, Switch } from "react-router-dom";
 import Login from "./views/login";
 import Authenticated from "./views/authenticated";
+import UpdateSbankenSecrets from "./views/updateSbankenSecrets";
 
 function App() {
     return (
         <Switch>
             <Route exact path="/" component={Login} />
             <Route path="/authenticated" component={Authenticated} />
+            <Route path="/settings" component={UpdateSbankenSecrets} />
         </Switch>
     );
 }
---
+++ src/views/updateSbankenSecrets.js
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+import { Form, Input, Button } from 'antd';
+
+const StyledForm = styled(Form)`
+    margin-top: 50px;
+    width: 500px;
+`;
+
+function UpdateSbankenSecrets() {
+    const onFinish = (values) => {
+        fetch(`${process.env.REACT_APP_BACKEND_URL}/user/secrets/sbanken`, {
+            method: 'PUT',
+            credentials: 'include',
+            body: JSON.stringify(values)
+        });
+    };
+
+    return (
+        <StyledForm
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+        >
+            <Form.Item label="Sbanken ClientID" name="sbanken_client_id">
+                <Input />
+            </Form.Item>
+
+            <Form.Item label="Sbanken Client Secret" name="sbanken_client_secret">
+                <Input.Password />
+            </Form.Item>
+
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                <Button type="primary" htmlType="submit">
+                    Update
+                </Button>
+            </Form.Item>
+        </StyledForm>
+    );
+};
+
+export default UpdateSbankenSecrets;
