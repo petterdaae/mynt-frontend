fa9dd45029515d9c08763d335753665cdd6cdc31
Petter Daae <petter.daae@gmail.com>
2021-08-23 10:48:40 +0200

    Add first version of transactions list to dashboard

--- src/views/dashboard.js
+++ src/views/dashboard.js
@@ -1,5 +1,34 @@
+import { Table } from "../components";
+import { useState, useEffect } from "react";
+
 function Dashboard() {
-  return <h1>Dashboard</h1>;
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(async () => {
+    fetch(`${process.env.REACT_APP_BACKEND_URL}/transactions`, {
+      credentials: "include",
+    })
+      .then((res) => res.json())
+      .then((data) =>
+        setTransactions(
+          data.map((transaction) => ({
+            accounting_date: transaction.accounting_date,
+            text: transaction.text,
+            amount: transaction.amount,
+          }))
+        )
+      );
+  }, [setTransactions]);
+
+  return (
+    <>
+      <h1>Dashboard</h1>
+      <Table
+        headers={["Accounting Date", "Text", "Amount"]}
+        data={transactions}
+      />
+    </>
+  );
 }
 
 export default Dashboard;
