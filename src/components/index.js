9e1cdc3e4d95ec8dd4726304cbc4b49c1aa4ad57
Petter Daae <petter.daae@gmail.com>
2021-09-15 14:23:40 +0200

    Add warning to delete all data

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import AccountCard from "./account_card";
 import Button from "./button";
 import Input from "./input";
 import TransactionList from "./transaction_list";
+import Modal from "./modal";
 
-export { Button, Input, AccountCard, TransactionList };
+export { Button, Input, AccountCard, TransactionList, Modal };
