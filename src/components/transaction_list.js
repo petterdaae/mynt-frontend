1ecb727d38cfa52dd9f408dc7f3bd036de935ebf
Petter Daae <petter.daae@gmail.com>
2021-09-17 18:47:09 +0200

    Tweak transaction list styling

--- src/components/transaction_list.js
+++ src/components/transaction_list.js
@@ -4,15 +4,28 @@ import { formatCurrency } from "../utils/currency";
 import { green, red } from "./color";
 import List from "./list";
 import ListItem from "./list_item";
+import RandomIcon from "./random_icon";
 
 const StyledListItem = styled(ListItem)`
   display: flex;
   justify-content: space-between;
 `;
 
-const DateTime = styled.div``;
-const Text = styled.div``;
+const DateTime = styled.div`
+  display: flex;
+  flex: 1;
+  align-items: center;
+`;
+
+const Text = styled.div`
+  display: flex;
+  align-items: center;
+  flex: 2;
+`;
+
 const Amount = styled.div`
+  display: flex;
+  align-items: center;
   ${(props) => props.color && `color: ${props.color};`}
 `;
 
@@ -22,6 +35,7 @@ function TransactionList({ data, className }) {
       <List>
         {data.map((item) => (
           <StyledListItem key={item.id}>
+            <RandomIcon />
             <DateTime>{item.accounting_date}</DateTime>
             <Text>{item.text}</Text>
             <Amount color={item.amount < 0 ? red : green}>
