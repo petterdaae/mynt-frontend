ec22291b61f18ccfd085279e6480e2423f3d4872
Petter Daae <petter.daae@gmail.com>
2021-09-27 18:45:28 +0200

    First version of spendingslist

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,9 +1,12 @@
 import { useCategories, CategoriesProvider } from "./useCategories";
 import { useTransactions, TransactionsProvider } from "./useTransactions";
+import { useSpendings, SpendingsProvider } from "./useSpendings";
 
 export {
   useCategories,
   useTransactions,
+  useSpendings,
   TransactionsProvider,
   CategoriesProvider,
+  SpendingsProvider,
 };
--- src/views/index.js
+++ src/views/index.js
@@ -3,5 +3,6 @@ import Authenticated from "./authenticated";
 import Transactions from "./transactions";
 import Settings from "./settings";
 import Categories from "./categories";
+import Spendings from "./spendings";
 
-export { Login, Authenticated, Transactions, Settings, Categories };
+export { Login, Authenticated, Transactions, Settings, Categories, Spendings };
