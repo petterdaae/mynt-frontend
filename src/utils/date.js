3b4246afd21586aa1f2ff9cb1c9dc24c4811ef41
Petter Daae <petter.daae@gmail.com>
2021-10-24 17:13:28 +0200

    Fix dateformat bug

---
+++ src/utils/date.js
@@ -0,0 +1,11 @@
+const formatDate = (date) => {
+  return (
+    date.getFullYear() +
+    "-" +
+    (date.getMonth() + 1).toString().padStart(2, "0") +
+    "-" +
+    date.getDate().toString().padStart(2, "0")
+  );
+};
+
+export { formatDate };
