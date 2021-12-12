53d12c0813c6db87c7ee0aec3b2ce4ad33603c67
Petter Daae <petter.daae@gmail.com>
2021-12-12 23:52:20 +0100

    Add loading spinner

--- src/domain/TransactionList/TransactionList.jsx
+++ src/domain/TransactionList/TransactionList.jsx
@@ -1,11 +1,14 @@
 import Transaction from "./Transaction";
 import { useTransactions } from "../../hooks/domain/useTransactions";
-import { Divider } from "@chakra-ui/layout";
+import { Divider, Spinner, Center } from "@chakra-ui/react";
 
 function TransactionList() {
   const { transactions, loading } = useTransactions();
-  return (
-    !loading &&
+  return loading ? (
+    <Center mt="8">
+      <Spinner size="xl" />
+    </Center>
+  ) : (
     transactions.map((transaction) => (
       <div key={transaction.id}>
         <Transaction transaction={transaction} />
