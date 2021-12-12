7cb4bd8b724c570d1304c4b27e30dd99d0815dea
Petter Daae <petter.daae@gmail.com>
2021-12-12 13:55:50 +0100

    Add functionality to time filter

--- src/domain/TransactionList/TransactionList.jsx
+++ src/domain/TransactionList/TransactionList.jsx
@@ -1,20 +1,9 @@
 import Transaction from "./Transaction";
 import { useTransactions } from "../../hooks/domain/useTransactions";
-import { useEffect } from "react";
-import { formatDate } from "../../utils/date";
 import { Divider } from "@chakra-ui/layout";
 
 function TransactionList() {
-  const { transactions, setFromAndToDate, loading } = useTransactions();
-  const today = formatDate(new Date());
-
-  useEffect(() => {
-    let fromDate = new Date();
-    fromDate.setMonth(fromDate.getMonth() - 1);
-    fromDate = formatDate(fromDate);
-    setFromAndToDate(fromDate, today);
-  }, []);
-
+  const { transactions, loading } = useTransactions();
   return (
     !loading &&
     transactions.map((transaction) => (
--- src/domain/TransactionList/TransactionListWithFilters.jsx
+++ src/domain/TransactionList/TransactionListWithFilters.jsx
@@ -1,21 +1,44 @@
 import { HStack, Checkbox, Divider, Select } from "@chakra-ui/react";
 import TransactionList from "./TransactionList";
+import { useTransactions } from "../../hooks/domain/useTransactions";
+import { useEffect, useState } from "react";
+import { formatDate } from "../../utils/date";
 
 function TransactionListWithFilters() {
+  const { setFromAndToDate, loading } = useTransactions();
+  const [monthsBack, setMonthsBack] = useState(1);
+  const today = formatDate(new Date());
+
+  useEffect(() => {
+    console.log("useEffect");
+    let fromDate = new Date();
+    fromDate.setMonth(fromDate.getMonth() - monthsBack);
+    fromDate = formatDate(fromDate);
+    setFromAndToDate(fromDate, today);
+  }, [monthsBack, setFromAndToDate]);
+
   return (
-    <>
-      <HStack spacing="16px">
-        <Select defaultValue={1}>
-          <option value={1}>Last 30 days</option>
-          <option value={2}>Last 60 days</option>
-          <option value={12}>Last year</option>
-          <option value={1000}>All time</option>
-        </Select>
-        <Checkbox size="lg">Uncategorized</Checkbox>
-      </HStack>
-      <Divider mt="8px" mb="8px" />
-      <TransactionList />
-    </>
+    !loading && (
+      <>
+        <HStack spacing="16px">
+          <Select
+            value={monthsBack}
+            onChange={(e) => {
+              console.log(e.target.value);
+              setMonthsBack(e.target.value);
+            }}
+          >
+            <option value={1}>Last 30 days</option>
+            <option value={2}>Last 60 days</option>
+            <option value={12}>Last year</option>
+            <option value={1000}>All time</option>
+          </Select>
+          <Checkbox size="lg">Uncategorized</Checkbox>
+        </HStack>
+        <Divider mt="8px" mb="8px" />
+        <TransactionList />
+      </>
+    )
   );
 }
 
