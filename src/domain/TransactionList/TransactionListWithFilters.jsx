1316f81d54746b492d9f5b14763db2e567f5625d
Petter Daae <petter.daae@gmail.com>
2021-12-12 14:46:50 +0100

    Add type checkbox functionality

--- src/domain/TransactionList/TransactionListWithFilters.jsx
+++ src/domain/TransactionList/TransactionListWithFilters.jsx
@@ -5,8 +5,9 @@ import { useEffect, useState } from "react";
 import { formatDate } from "../../utils/date";
 
 function TransactionListWithFilters() {
-  const { setFromAndToDate, loading } = useTransactions();
+  const transactions = useTransactions();
   const [monthsBack, setMonthsBack] = useState(1);
+  const [showCategorized, setShowCategorized] = useState(true);
   const today = formatDate(new Date());
 
   useEffect(() => {
@@ -14,31 +15,39 @@ function TransactionListWithFilters() {
     let fromDate = new Date();
     fromDate.setMonth(fromDate.getMonth() - monthsBack);
     fromDate = formatDate(fromDate);
-    setFromAndToDate(fromDate, today);
-  }, [monthsBack, setFromAndToDate]);
+    transactions.setFromAndToDate(fromDate, today);
+  }, [monthsBack, transactions.setFromAndToDate]);
+
+  useEffect(() => {
+    transactions.setType(showCategorized ? "all" : "uncategorized");
+  }, [showCategorized, transactions.setType]);
 
   return (
-    !loading && (
-      <>
-        <HStack spacing="16px">
-          <Select
-            value={monthsBack}
-            onChange={(e) => {
-              console.log(e.target.value);
-              setMonthsBack(e.target.value);
-            }}
-          >
-            <option value={1}>Last 30 days</option>
-            <option value={2}>Last 60 days</option>
-            <option value={12}>Last year</option>
-            <option value={1000}>All time</option>
-          </Select>
-          <Checkbox size="lg">Uncategorized</Checkbox>
-        </HStack>
-        <Divider mt="8px" mb="8px" />
-        <TransactionList />
-      </>
-    )
+    <>
+      <HStack spacing="16px">
+        <Select
+          value={monthsBack}
+          onChange={(e) => {
+            console.log(e.target.value);
+            setMonthsBack(e.target.value);
+          }}
+        >
+          <option value={1}>Last 30 days</option>
+          <option value={2}>Last 60 days</option>
+          <option value={12}>Last year</option>
+          <option value={1000}>All time</option>
+        </Select>
+        <Checkbox
+          size="lg"
+          isChecked={showCategorized}
+          onChange={(e) => setShowCategorized((prev) => !prev)}
+        >
+          Categorized
+        </Checkbox>
+      </HStack>
+      <Divider mt="8px" mb="8px" />
+      <TransactionList />
+    </>
   );
 }
 
