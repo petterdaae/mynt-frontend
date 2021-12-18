a43d9dd90c8650cf24b093cde3837df5171c1c63
Petter Daae <petter.daae@gmail.com>
2021-12-18 14:54:53 +0100

    Rewrite login page

--- src/login.js
+++ src/login.js
@@ -1,42 +1,37 @@
-import styled from "styled-components";
-import { Button } from "./components";
-
-const Wrapper = styled.div`
-  text-align: center;
-`;
-
-const StyledButton = styled(Button)`
-  display: inline-block;
-  margin-top: 200px;
-  margin-right: 20px;
-`;
+import { Button, Center, Text, HStack, VStack } from "@chakra-ui/react";
 
 function Login() {
   return (
-    <Wrapper>
-      <StyledButton
-        type="primary"
-        size="large"
-        onClick={() => {
-          window.location.replace(
-            `${process.env.REACT_APP_BACKEND_URL}/auth/redirect`
-          );
-        }}
-      >
-        Sign in
-      </StyledButton>
-      <StyledButton
-        type="primary"
-        size="large"
-        onClick={() => {
-          window.location.replace(
-            `${process.env.REACT_APP_BACKEND_URL}/auth/demo`
-          );
-        }}
-      >
-        Demo
-      </StyledButton>
-    </Wrapper>
+    <Center mt="32">
+      <VStack>
+        <Text fontSize="4xl" as="u" mb="4">
+          Mynt
+        </Text>
+        <HStack>
+          <Button
+            colorScheme="blue"
+            onClick={() => {
+              window.location.replace(
+                `${process.env.REACT_APP_BACKEND_URL}/auth/redirect`
+              );
+            }}
+          >
+            Sign in
+          </Button>
+          <Button
+            ml="4"
+            variant="outline"
+            onClick={() => {
+              window.location.replace(
+                `${process.env.REACT_APP_BACKEND_URL}/auth/demo`
+              );
+            }}
+          >
+            Demo
+          </Button>
+        </HStack>
+      </VStack>
+    </Center>
   );
 }
 
