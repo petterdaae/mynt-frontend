4f6f4f15805319c13d12b3db6e2903bbbac13acf
Petter Daae <petter.daae@gmail.com>
2021-12-18 14:48:00 +0100

    Replace navigation with chakra tabs

--- src/authenticated.js
+++ src/authenticated.js
@@ -2,73 +2,17 @@ import styled from "styled-components";
 import { useHistory, useLocation } from "react-router-dom";
 import PropTypes from "prop-types";
 import { base, breakpoint } from "./components/size";
-import { mainFontColor, weakFontColor } from "./components/color";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Tabs, TabList, Tab } from "@chakra-ui/react";
 
 const ContentWrapper = styled.div`
   width: ${breakpoint}px;
   margin: auto;
 
-  margin-top: ${10 * base}px;
-  padding: ${8 * base}px;
-
   @media (max-width: ${breakpoint}px) {
     width: auto;
-    margin-top: ${14 * base}px;
     padding: ${2 * base}px;
   }
-`;
-
-const NavigationWrapper = styled.div`
-  overflow: hidden;
-  position: fixed;
-  top: 0;
-  width: 100%;
-
-  background: white;
-  border-bottom: 1px solid lightgray;
-  display: flex;
-`;
-
-const Navigation = styled.div`
-  width: ${breakpoint}px;
-  margin: auto;
-  @media (max-width: ${breakpoint}px) {
-    width: auto;
-  }
-
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  height: ${14 * base}px;
-`;
-
-const StyledNavigationItem = styled.div`
-  color: ${weakFontColor};
-  margin-left: ${base}px;
-  margin-right: ${base}px;
-
-  &:first-child {
-    margin-left: ${8 * base}px;
-  }
-
-  &:last-child {
-    margin-right: ${8 * base}px;
-  }
-
-  &:hover {
-    color: ${mainFontColor};
-    cursor: pointer;
-    text-decoration: underline;
-  }
-
-  ${(props) => {
-    if (props.active) {
-      return `
-        color: ${mainFontColor};
-      `;
-    }
-  }}
 `;
 
 const Content = styled.div`
@@ -103,19 +47,51 @@ function Authenticated({ children }) {
     };
   }, []);
 
+  const [tabIndex, setTabIndex] = useState(0);
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === "/authenticated/transactions") {
+      setTabIndex(0);
+    } else if (location.pathname === "/authenticated/categories") {
+      setTabIndex(1);
+    } else if (location.pathname === "/authenticated/spendings") {
+      setTabIndex(2);
+    } else if (location.pathname === "/authenticated/settings") {
+      setTabIndex(3);
+    }
+  }, [location]);
+
   return (
     <>
-      <NavigationWrapper>
-        <Navigation>
+      <Tabs variant="enclosed" mt="2" mb="4" index={tabIndex} align="center">
+        <TabList>
           <NavigationItem
             path="/authenticated/transactions"
             name="Transactions"
+            setTabIndex={setTabIndex}
+            tabIndex={0}
           />
-          <NavigationItem path="/authenticated/categories" name="Categories" />
-          <NavigationItem path="/authenticated/spendings" name="Spendings" />
-          <NavigationItem path="/authenticated/settings" name="Settings" />
-        </Navigation>
-      </NavigationWrapper>
+          <NavigationItem
+            path="/authenticated/categories"
+            name="Categories"
+            setTabIndex={setTabIndex}
+            tabIndex={1}
+          />
+          <NavigationItem
+            path="/authenticated/spendings"
+            name="Spendings"
+            setTabIndex={setTabIndex}
+            tabIndex={2}
+          />
+          <NavigationItem
+            path="/authenticated/settings"
+            name="Settings"
+            setTabIndex={setTabIndex}
+            tabIndex={3}
+          />
+        </TabList>
+      </Tabs>
       <ContentWrapper>
         <Content>{children}</Content>
       </ContentWrapper>
@@ -133,23 +109,27 @@ Authenticated.propTypes = {
   children: PropTypes.node,
 };
 
-function NavigationItem({ path, name }) {
+function NavigationItem({ path, name, setTabIndex, tabIndex }) {
   const history = useHistory();
-  const location = useLocation();
 
   return (
-    <StyledNavigationItem
-      onClick={() => history.push(path)}
+    <Tab
+      onClick={() => {
+        setTabIndex(tabIndex);
+        history.push(path);
+      }}
       active={location.pathname === path}
     >
       {name}
-    </StyledNavigationItem>
+    </Tab>
   );
 }
 
 NavigationItem.propTypes = {
   path: PropTypes.string,
   name: PropTypes.string,
+  setTabIndex: PropTypes.func,
+  tabIndex: PropTypes.number,
 };
 
 export default Authenticated;
