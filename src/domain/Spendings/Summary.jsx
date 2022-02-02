51f70b02bab3e9feb73db78ec1183abc48aa7d18
Petter Daae <petter.daae@gmail.com>
2022-02-02 19:17:49 +0100

    Add custom barchart

--- src/domain/Spendings/Summary.jsx
+++ src/domain/Spendings/Summary.jsx
@@ -1,56 +1,6 @@
 import PropTypes from "prop-types";
-import { Center, Badge, HStack, VStack } from "@chakra-ui/react";
-import { BarChart, Bar } from "recharts";
-
-const data = [
-  {
-    name: "Page A",
-    actualIncome: 23639,
-    budgetIncome: 8600,
-    unexpectedIncome: 3000,
-    actualExpenses: 17839,
-    budgetExpenses: 19573,
-    unexpectedExpenses: 2347,
-  },
-  // {
-  //   name: "Page B",
-  //   uv: 3000,
-  //   pv: 1398,
-  //   amt: 2210,
-  // },
-  // {
-  //   name: "Page C",
-  //   uv: 2000,
-  //   pv: 9800,
-  //   amt: 2290,
-  // },
-];
-
-function Example() {
-  return (
-    <BarChart
-      width={350}
-      height={200}
-      data={data}
-      margin={{
-        top: 5,
-        right: 30,
-        left: 20,
-        bottom: 5,
-      }}
-    >
-      <Bar dataKey="actualIncome" fill="#68D391" stackId="a" />
-      <Bar dataKey="unexpectedIncome" fill="#F6AD55" stackId="a" />
-      <Bar dataKey="budgetIncome" fill="#63b3ed" />
-
-      <Bar dataKey="spacing" fill="red" />
-
-      <Bar dataKey="actualExpenses" fill="#FC8181" stackId="b" />
-      <Bar dataKey="unexpectedExpenses" fill="#F6AD55" stackId="b" />
-      <Bar dataKey="budgetExpenses" fill="#63b3ed" />
-    </BarChart>
-  );
-}
+import { Center, HStack, VStack } from "@chakra-ui/react";
+import Bar from "./Bar";
 
 function Summary({ currentCategory, spendings }) {
   // const spending = spendings.find((s) => s.category.id === currentCategory) ?? {
@@ -62,33 +12,21 @@ function Summary({ currentCategory, spendings }) {
     <Center m="2">
       <VStack>
         <HStack>
-          <HStack align="left">
-            <VStack align="left">
-              <Badge colorScheme="green">Actual</Badge>
-              <Badge colorScheme="orange">Unexpected</Badge>
-              <Badge colorScheme="blue">Budget</Badge>
-            </VStack>
-            <VStack align="right">
-              <Badge colorScheme="green">17 839,60</Badge>
-              <Badge colorScheme="orange">1 534,20</Badge>
-              <Badge colorScheme="blue">19 573,00</Badge>
-            </VStack>
-          </HStack>
-
-          <Example />
-
-          <HStack align="left">
-            <VStack align="left">
-              <Badge colorScheme="red">Actual</Badge>
-              <Badge colorScheme="orange">Unexpected</Badge>
-              <Badge colorScheme="blue">Budget</Badge>
-            </VStack>
-            <VStack align="right">
-              <Badge colorScheme="red">-17 839,60</Badge>
-              <Badge colorScheme="orange">-1 534,20</Badge>
-              <Badge colorScheme="blue">-19 573,00</Badge>
-            </VStack>
-          </HStack>
+          <Bar
+            max={23456}
+            height={200}
+            width={85}
+            actual={19234}
+            budget={23456}
+          />
+          <Bar
+            max={23456}
+            height={200}
+            width={85}
+            actual={9345}
+            budget={8345}
+            isIncome
+          />
         </HStack>
       </VStack>
     </Center>
