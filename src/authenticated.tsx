adc843436a983e9593c43002619f8ae2642d63aa
Petter Daae <petter.daae@gmail.com>
2022-07-16 11:53:41 +0200

    Increase navbar z-index

--- src/authenticated.tsx
+++ src/authenticated.tsx
@@ -31,7 +31,7 @@ const NavWrapper = styled.div`
   position: fixed;
   top: 0;
   width: 100%;
-  z-index: 1;
+  z-index: 2;
   background-color: white;
   box-shadow: rgba(0, 0, 0, 0.05) 0px 0px 0px 1px,
     rgb(209, 213, 219) 0px 0px 0px 1px inset;
